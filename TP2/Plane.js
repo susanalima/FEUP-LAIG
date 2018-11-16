4cd4f5288799d0a7bfe7f29e4cef2353fc42569d
Afonso <afonsojoseazevedo@gmail.com>
2018-11-16 11:01:43 +0000

    Create Plane.js

---
+++ TP2/Plane.js
@@ -0,0 +1,11 @@
+class Plane extends CGFobject{
+    constructor(scene, div_U, div_V, degree_U, degree_V){
+        
+        this.scene = scene;
+        this.div_U = div_U;
+        this.div_V = div_V;
+        this.degree_U = degree_U;
+        this.degree_V = degree_V;
+    }
+
+}
