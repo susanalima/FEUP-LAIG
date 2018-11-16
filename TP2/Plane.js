37605a99370d1c81517d9048ab32f8f7b04be630
Afonso <afonsojoseazevedo@gmail.com>
2018-11-16 11:42:54 +0000

    Update Plane.js

--- TP2/Plane.js
+++ TP2/Plane.js
@@ -1,11 +1,8 @@
 class Plane extends CGFobject{
-    constructor(scene, div_U, div_V, degree_U, degree_V){
-        
+    constructor(scene, nparts_u, nparts_v){
         this.scene = scene;
-        this.div_U = div_U;
-        this.div_V = div_V;
-        this.degree_U = degree_U;
-        this.degree_V = degree_V;
+        this.nparts_u = nparts_u;
+        this.nparts_v = nparts_v;
+        this.controlPoints;
     }
-
 }
