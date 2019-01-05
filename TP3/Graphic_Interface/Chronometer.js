4f160a99bc4cb195c59be84ad777db6d8c11089a
Unknown <up201603634@fe.up.pt>
2019-01-05 22:36:03 +0000

    3d marker and chronometer

--- TP3/Graphic_Interface/Chronometer.js
+++ TP3/Graphic_Interface/Chronometer.js
@@ -16,6 +16,9 @@ class Chronometer extends CGFobject{
         this.text2 = text2;
 
         this.clockPointer = new MyTriangle(this.scene, -0.5,-6,0,0,6,0,0.5,-6,0);
+        this.back = new MyCylinder(scene, 40, 1, 10.5, 10.5, 5),
+        this.support_back = new MyCylinder(this.scene, 4, 1,4,4, 3.5);
+
         this.clock = new MyCylinderBase(this.scene, 40, 10);
     }
 
@@ -50,17 +53,23 @@ class Chronometer extends CGFobject{
         this.scene.translate(this.x, this.y,this.z);
         this.scene.rotate( Math.PI/3, 0,1,0);
         this.scene.rotate( Math.PI/4, 1,0,0);
-        this.scene.scale(0.5,0.5,0.5);
+        this.scene.scale(0.4,0.4,0.4);
         this.scene.translate(10,0,0);
         
-
+        this.scene.pushMatrix();
+        this.scene.translate(0,0,0.2);
+        this.back.display();
+        this.scene.translate(0,-2,8);
+        this.scene.rotate(Math.PI/2,1,0,0);
+        this.support_back.display();
+        this.scene.popMatrix();
 
         this.scene.pushMatrix();   
         this.scene.scale(-1,1,1);
         this.scene.rotate(Math.PI, 0,1,0);
         this.scene.rotate(Math.PI/4, 0,0,1);
         this.scene.rotate(Math.PI/4, 0,0,1);
-        
+    
         this.text2.bind();
         this.clock.display();
         this.scene.popMatrix();
